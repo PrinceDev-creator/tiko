49397a49c00270908a3d20ec4debe5308e539901
Prince BOGNONKPEclear <princedev317@gmail.com>
2025-12-18 18:34:06 +0100

    chore: fully comment of server.js for permit to deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,22 +9,22 @@
 // const server = http.createServer(app)
 // server.listen(process.env.PORT || 3000, HOSTNAME)
 
-require('dotenv').config();
+// require('dotenv').config();
 
-const app = require('./app');
-const https = require('https');
-const fs = require('fs');
+// const app = require('./app');
+// const https = require('https');
+// const fs = require('fs');
 
-const HOSTNAME = "192.168.1.196";
-const PORT = process.env.PORT || 3000;
+// const HOSTNAME = "192.168.1.196";
+// const PORT = process.env.PORT || 3000;
 
-const options = {
-    key: fs.readFileSync('./certs/localhost-key.pem'),
-    cert: fs.readFileSync('./certs/localhost.pem'),
-};
+// // const options = {
+// //     key: fs.readFileSync('./certs/localhost-key.pem'),
+// //     cert: fs.readFileSync('./certs/localhost.pem'),
+// // };
 
-const server = https.createServer(options, app);
+// const server = https.createServer(options, app);
 
-server.listen(PORT, HOSTNAME, () => {
-    console.log(`HTTPS server running on https://${HOSTNAME}:${PORT}`);
-});
+// server.listen(PORT, HOSTNAME, () => {
+//     console.log(`HTTPS server running on https://${HOSTNAME}:${PORT}`);
+// });
