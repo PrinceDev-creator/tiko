c4289f911b8371e8816b79edbbc1a9eba08227cc
Prince BOGNONKPEclear <princedev317@gmail.com>
2025-12-18 15:53:13 +0100

    fix(tests): return to a minimal file configuration

--- tests/vite.config.js
+++ tests/vite.config.js
@@ -1,20 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs'
+//import fs from 'fs'
 
 export default defineConfig({
     plugins: [react()],
-    server: {
-        host: true, // nécessaire pour accéder via IP locale ou Ngrok
-        port: 5173,
-        allowedHosts: [
-            '*'
-            //'intramuscular-angelena-subdendroid.ngrok-free.dev'
-        ],
-        //si tu utilises HTTPS local :
-        https: {
-            key: fs.readFileSync('./certs/localhost-key.pem'),
-            cert: fs.readFileSync('./certs/localhost.pem'),
-        }
-    }
+    // server: {
+    //     host: true, // nécessaire pour accéder via IP locale ou Ngrok
+    //     port: 5173,
+    //     allowedHosts: [
+    //         '*'
+    //         //'intramuscular-angelena-subdendroid.ngrok-free.dev'
+    //     ],
+    //     //si tu utilises HTTPS local :
+    //     https: {
+    //         key: fs.readFileSync('./certs/localhost-key.pem'),
+    //         cert: fs.readFileSync('./certs/localhost.pem'),
+    //     }
+    // }
 })
