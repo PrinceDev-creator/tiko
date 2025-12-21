8652aa0119610891635506c729e7fcf8d597b5c2
Prince BOGNONKPEclear <princedev317@gmail.com>
2025-12-21 14:03:49 +0100

    fix: modify https methods get to post on route 'verify-pass/:passId'

--- src/routes/pass.js
+++ src/routes/pass.js
@@ -5,6 +5,6 @@ const passCtrl = require('../controllers/pass')
 
 router.get('/:eventId', auth, passCtrl.getAllPasses)
 router.get('/create-pass/:passInfosId', auth, passCtrl.createPass)
-router.get('/verify-pass/:passId', auth, passCtrl.verifyPass)
+router.post('/verify-pass/:passId', auth, passCtrl.verifyPass)
 
 module.exports = router
