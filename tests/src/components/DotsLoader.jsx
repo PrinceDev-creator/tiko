4190dfdc88725ee6d2f2bce7d428b50e2e45737b
Prince BOGNONKPEclear <princedev317@gmail.com>
2026-01-03 14:07:29 +0100

    feat: add Loder file

---
+++ tests/src/components/DotsLoader.jsx
@@ -0,0 +1,13 @@
+import "../styles/loading.css"
+
+function DotsLoader() {
+    return (
+        <div className="dots">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    );
+}
+
+export default DotsLoader;
